182f7c9c9c88a10a686a7e43fc6210a04dec2789
JB <sebastian03741@gmail.com>
2025-07-29 20:26:42 -0500

    29/07

--- 1. HelloWorld/#1_PrintVariables.js
+++ 1. HelloWorld/#1_PrintVariables.js
@@ -32,4 +32,20 @@ name = 3.5 // This re-assigns the value of "name" to 3.5, which is a number.
 console.log(name); // This will print "3.5" to the console.
 // As you may thought, it's not possible to re-assign a value to a constant variable, like 'pi'.
 
+
+// A variable can hold different types of value:
+// It can be Numbers like integers or decimals.
+let number1=3, number2=2.7
+
+// It can be Strings.. (The value or "message" needs to be wrapped either in double quoutes "" or single quotes '')
+const greet="Hello world!", salute='Hello, Mr.World!'
+
+// ..Boolean values.
+let isThisAString=false
+const isThisABool=true
+
+// A variable automatically receives an 'undefined' value if we don't assign them anything:
+let nothing;
+console.log(nothing) // Output: undefined
+
 // Finally, in JavaScript it's not necessary to put a `;` at the end of any line.
