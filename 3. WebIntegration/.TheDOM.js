56924cb0931a3039c62fdaab49541c4ece06e51d
JB <sebastian03741@gmail.com>
2025-08-05 21:04:27 -0500

    5/08.r

--- 3. WebIntegration/.TheDOM.js
+++ 3. WebIntegration/.TheDOM.js
@@ -7,19 +7,19 @@
  it's useful to store HTML elements in JavaScript variables, like this:
  const/let variableName = document.querySelector("Markup, #id or .class");
 
- const body = document.querySelector("body");
- const coonImage = document.querySelector("#coon-image");
- const flowers = document.querySelector(.flowers);
+const body = document.querySelector("body");
+const coonImage = document.querySelector("#coon-image");
+const flowers = document.querySelector(.flowers);
 
  We can now use the HTML elements in our JS code as variables.
  And these variables now have special methods that helps us modify our html and css properties.
  Here are some:
 
- coonImage.src="new-image.jpg" <--We access our coonImage 'src' property and change it to "new-image.jpg"
- divContainer.style.backgroundColor="green" <--Accessing 'style' then 'background-color' to change it to "Darkgreen"
+coonImage.src="new-image.jpg" <--We access our coonImage 'src' property and change it to "new-image.jpg"
+divContainer.style.backgroundColor="green" <--Accessing 'style' then 'background-color' to change it to "Darkgreen"
  
-There's a special method that needs user activity to be activated, this is, event listeners.
-Things like hover or clicking an element are called events. We can give advise of user activity to event listeners:
+ There's a special method that needs user activity to be activated, this is, event listeners.
+ Things like hover or clicking an element are called events. We can give advise of user activity to event listeners:
 
 const button=querySelector(#first-button);
 button.addEventListener("click", ()=>{
